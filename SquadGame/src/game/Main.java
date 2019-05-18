6e06fcdd9114c02aab68e756b7a2440bd4f3b7fe
dsamuel27 <david.samuel1227@gmail.com>
2019-05-18 11:25:20 -0700

    Can load images on screen now.

--- SquadGame/src/game/Main.java
+++ SquadGame/src/game/Main.java
@@ -1,6 +1,11 @@
 package game;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,9 +21,21 @@ public class Main extends Application {
 		window.setTitle(gameName);
 		window.setHeight(SIZE);
 		window.setWidth(SIZE);
+		
+		
+		Group group = new Group();
+		Canvas gameCanvas = new Canvas(SIZE,SIZE);
+		GraphicsContext context = gameCanvas.getGraphicsContext2D();
+		Image image = new Image("Sprites/testImage.png");
+		context.drawImage(image, 5,5,64,48);
+		
+		
+		Scene scene = new Scene(group,SIZE,SIZE);
+		
+		group.getChildren().add(gameCanvas);
+		
+		window.setScene(scene);
 		window.show();
-		
-		
 		
 	}
 	
